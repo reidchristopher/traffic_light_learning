10ff15db1fdc912f51ba5b6763a623deef179ec6
reidchristopher <reid.christopher@yahoo.com>
2019-11-19 17:24:20 -0800

    Added gradient descent training method to network

--- tf_network.py
+++ tf_network.py
@@ -6,7 +6,6 @@ import numpy as np
 
 
 class TFNetwork:
-
     session = tf.compat.v1.Session()
 
     def __init__(self, input_size, output_size, hidden_layer_size, num_hidden_layers):
@@ -34,10 +33,11 @@ class TFNetwork:
                 inputs = self.hidden_outputs[i - 1]
 
             self.hidden_weights[i] = tf.Variable(tf.random.normal([num_inputs, hidden_layer_size], stddev=0.05),
-                                            name="hw%d" % i)
+                                                 name="hw%d" % i)
             self.hidden_biases[i] = tf.Variable(tf.random.normal([hidden_layer_size], stddev=0.05), name="hb%d" % i)
 
-            self.hidden_outputs[i] = tf.nn.relu(tf.add(tf.matmul(inputs, self.hidden_weights[i]), self.hidden_biases[i]))
+            self.hidden_outputs[i] = tf.nn.relu(
+                tf.add(tf.matmul(inputs, self.hidden_weights[i]), self.hidden_biases[i]))
 
         self.output_weights = tf.Variable(tf.random.normal([hidden_layer_size, output_size], stddev=0.05), name="ow")
         self.output_biases = tf.Variable(tf.random.normal([output_size], stddev=0.05), name="ob")
@@ -53,7 +53,6 @@ class TFNetwork:
         new_network = TFNetwork(self.input_size, self.output_size, self.hidden_layer_size, self.num_hidden_layers)
 
         for i in range(self.num_hidden_layers):
-
             copy_op = new_network.hidden_weights[i].assign(self.hidden_weights[i])
 
             new_network.session.run(copy_op)
@@ -72,15 +71,14 @@ class TFNetwork:
 
         return new_network
 
-    def mutate(self, mutation_fraction, stddev): # other args? (noise distribution, etc)
+    def mutate(self, mutation_fraction, stddev):  # other args? (noise distribution, etc)
 
-        assert(0 < mutation_fraction <= 1)
+        assert (0 < mutation_fraction <= 1)
 
         nn_weights = self.hidden_weights + self.hidden_biases + [self.output_weights, self.output_biases]
 
         for weights in nn_weights:
-
-            mask = np.random.choice([0, 1], weights.shape, replace=True, p=[(1-mutation_fraction), mutation_fraction])
+            mask = np.random.choice([0, 1], weights.shape, replace=True, p=[(1 - mutation_fraction), mutation_fraction])
 
             noise = np.random.normal(scale=stddev, size=weights.shape)
 
@@ -88,39 +86,56 @@ class TFNetwork:
 
             self.session.run(assign_op)
 
+    def train(self, x, y, epochs, learning_rate=0.1, batch_size=None, print_cost=False):
+
+        assert(len(x) == len(y))
+        assert(len(x) == len(y))
+
+        if batch_size is None:
+            batch_size = len(x)
+
+        num_batches = int(len(x) / batch_size)
+
+        clipped_output = tf.clip_by_value(self.output, 1e-10, 0.9999999)
+        cost = -tf.reduce_mean(tf.reduce_sum(self.y * tf.math.log(clipped_output)
+                                             + (1 - self.y) * tf.math.log(1 - clipped_output), axis=1))
+
+        optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+        for epoch in range(epochs):
+            avg_cost = 0
+            for i in range(num_batches):
+                start_index = i * batch_size
+                end_index = min((i + 1) * batch_size, len(x))
+                batch_x, batch_y = x[start_index:end_index], y[start_index:end_index]
+                _, c = self.session.run([optimizer, cost],
+                                        feed_dict={self.x: batch_x, self.y: batch_y})
+                avg_cost += c / num_batches
+
+            if print_cost:
+                print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
+
 
 def test():
     # runs a test of the neural network constructed in the LearningPolicy object on the MNIST dataset
 
-    example = TFNetwork(28 * 28, 10, 100, 1)
-
-    clipped_output = tf.clip_by_value(example.output, 1e-10, 0.9999999)
-    cross_entropy = -tf.reduce_mean(tf.reduce_sum(example.y * tf.math.log(clipped_output)
-                                                  + (1 - example.y) * tf.math.log(1 - clipped_output), axis=1))
+    example = TFNetwork(28 * 28, 10, 100, 3)
 
     from tensorflow.examples.tutorials.mnist import input_data
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-    learning_rate = 0.5
+    learning_rate = 0.25
     epochs = 10
     batch_size = 100
-
-    optimiser = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
 
     correct_prediction = tf.equal(tf.argmax(example.y, 1), tf.argmax(example.output, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    total_batch = int(len(mnist.train.labels) / batch_size)
+    x_train, y_train = mnist.train.images, mnist.train.labels
 
-    for epoch in range(epochs):
-        avg_cost = 0
-        for i in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
-            _, c =example.session.run([optimiser, cross_entropy],
-                            feed_dict={example.x: batch_x, example.y: batch_y})
-            avg_cost += c / total_batch
-        print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
-    print(example.session.run(accuracy, feed_dict={example.x: mnist.test.images, example.y: mnist.test.labels}))
+    example.train(x_train, y_train, epochs, learning_rate, batch_size, print_cost=True)
+
+    print("MNIST accuracy: %.3f" % example.session.run(accuracy, feed_dict={example.x: mnist.test.images, example.y: mnist.test.labels}))
 
 
 if __name__ == "__main__":
