93e59b22006931ceec070998f41e782bc40459cc
reidchristopher <reid.christopher@yahoo.com>
2019-11-21 17:04:10 -0800

    Migrated to TensorFlow 2.0. Added saving and loading from file functionaliy

--- tf_network.py
+++ tf_network.py
@@ -6,136 +6,129 @@ import numpy as np
 
 
 class TFNetwork:
-    session = tf.compat.v1.Session()
 
-    def __init__(self, input_size, output_size, hidden_layer_size, num_hidden_layers):
+    @staticmethod
+    def from_existing(network):
 
-        self.input_size = input_size
-        self.output_size = output_size
-        self.hidden_layer_size = hidden_layer_size
-        self.num_hidden_layers = num_hidden_layers
+        return TFNetwork(None, None, None, None, network.model)
 
-        # initialize neural network
-        self.x = tf.compat.v1.placeholder(tf.float32, [None, input_size])
-        self.y = tf.compat.v1.placeholder(tf.float32, [None, output_size])
+    @staticmethod
+    def from_save(file_path):
 
-        self.hidden_weights = [None for i in range(num_hidden_layers)]
-        self.hidden_biases = [None for i in range(num_hidden_layers)]
+        model = tf.keras.models.load_model(file_path)
 
-        self.hidden_outputs = [None for i in range(num_hidden_layers)]
+        return TFNetwork(None, None, None, None, model)
 
-        for i in range(num_hidden_layers):
-            if i == 0:
-                num_inputs = input_size
-                inputs = self.x
-            else:
-                num_inputs = hidden_layer_size
-                inputs = self.hidden_outputs[i - 1]
+    def __init__(self, input_size, output_size, hidden_layer_size, num_hidden_layers, original_model=None):
 
-            self.hidden_weights[i] = tf.Variable(tf.random.normal([num_inputs, hidden_layer_size], stddev=0.05),
-                                                 name="hw%d" % i)
-            self.hidden_biases[i] = tf.Variable(tf.random.normal([hidden_layer_size], stddev=0.05), name="hb%d" % i)
+        if original_model is not None:
 
-            self.hidden_outputs[i] = tf.nn.relu(
-                tf.add(tf.matmul(inputs, self.hidden_weights[i]), self.hidden_biases[i]))
+            self.input_size = original_model.layers[0].input_shape[-1]
+            self.output_size = original_model.layers[-1].output_shape[-1]
+            self.hidden_layer_size = original_model.layers[0].output_shape[-1]
+            self.num_hidden_layers = len(original_model.layers) - 1
 
-        self.output_weights = tf.Variable(tf.random.normal([hidden_layer_size, output_size], stddev=0.05), name="ow")
-        self.output_biases = tf.Variable(tf.random.normal([output_size], stddev=0.05), name="ob")
+            self.model = tf.keras.models.clone_model(original_model)
 
-        self.output = tf.nn.softmax(tf.add(tf.matmul(self.hidden_outputs[-1], self.output_weights), self.output_biases))
+            for i in range(self.num_hidden_layers + 1):
 
-        # initialize global variables
-        global_init_op = tf.compat.v1.global_variables_initializer()
-        self.session.run(global_init_op)
+                original_layer = original_model.get_layer(index=i)
 
-    def get_copy(self):
+                copied_layer = self.model.get_layer(index=i)
 
-        new_network = TFNetwork(self.input_size, self.output_size, self.hidden_layer_size, self.num_hidden_layers)
+                copied_layer.set_weights(original_layer.get_weights())
 
-        for i in range(self.num_hidden_layers):
-            copy_op = new_network.hidden_weights[i].assign(self.hidden_weights[i])
+        else:
+            assert (num_hidden_layers > 0)
+            assert(input_size is not None)
+            assert(output_size is not None)
+            assert(hidden_layer_size is not None)
+            assert(num_hidden_layers is not None)
 
-            new_network.session.run(copy_op)
+            self.input_size = input_size
+            self.output_size = output_size
+            self.hidden_layer_size = hidden_layer_size
+            self.num_hidden_layers = num_hidden_layers
 
-            copy_op = new_network.hidden_biases[i].assign(self.hidden_biases[i])
+            self.model = tf.keras.Sequential()
 
-            new_network.session.run(copy_op)
+            # add hidden layers
+            for i in range(num_hidden_layers):
 
-        copy_op = new_network.output_weights.assign(self.output_weights)
+                if i == 0:
+                    input_shape = (input_size,)
+                else:
+                    input_shape = (hidden_layer_size,)
 
-        new_network.session.run(copy_op)
+                self.model.add(tf.keras.layers.Dense(hidden_layer_size, input_shape=input_shape, activation='relu'))
 
-        copy_op = new_network.output_biases.assign(self.output_biases)
+            # add output layer
+            self.model.add(tf.keras.layers.Dense(output_size, activation='softmax'))
 
-        new_network.session.run(copy_op)
-
-        return new_network
+            self.model.compile(optimizer=tf.keras.optimizers.SGD(0.1),
+                               loss='categorical_crossentropy',
+                               metrics=['accuracy'])
 
     def mutate(self, mutation_fraction, stddev):  # other args? (noise distribution, etc)
 
         assert (0 < mutation_fraction <= 1)
 
-        nn_weights = self.hidden_weights + self.hidden_biases + [self.output_weights, self.output_biases]
+        for i in range(self.num_hidden_layers + 1):
 
-        for weights in nn_weights:
-            mask = np.random.choice([0, 1], weights.shape, replace=True, p=[(1 - mutation_fraction), mutation_fraction])
+            layer = self.model.get_layer(index=i)
 
-            noise = np.random.normal(scale=stddev, size=weights.shape)
+            weights = layer.get_weights()
 
-            assign_op = weights.assign_add(mask * noise)
+            for j in range(len(weights)):
 
-            self.session.run(assign_op)
+                mask = np.random.choice([0, 1], weights[j].shape, replace=True, p=[(1 - mutation_fraction), mutation_fraction])
 
-    def train(self, x, y, epochs, learning_rate=0.1, batch_size=None, print_cost=False):
+                noise = np.random.normal(scale=stddev, size=weights[j].shape)
 
-        assert(len(x) == len(y))
+                weights[j] += mask * noise
+
+            layer.set_weights(weights)
+
+    def train(self, x, y, epochs, batch_size=None):
+
         assert(len(x) == len(y))
 
         if batch_size is None:
             batch_size = len(x)
 
-        num_batches = int(len(x) / batch_size)
+        self.model.fit(x, y, epochs=epochs, batch_size=batch_size)
 
-        clipped_output = tf.clip_by_value(self.output, 1e-10, 0.9999999)
-        cost = -tf.reduce_mean(tf.reduce_sum(self.y * tf.math.log(clipped_output)
-                                             + (1 - self.y) * tf.math.log(1 - clipped_output), axis=1))
+    def save(self, file_path):
 
-        optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
-
-        for epoch in range(epochs):
-            avg_cost = 0
-            for i in range(num_batches):
-                start_index = i * batch_size
-                end_index = min((i + 1) * batch_size, len(x))
-                batch_x, batch_y = x[start_index:end_index], y[start_index:end_index]
-                _, c = self.session.run([optimizer, cost],
-                                        feed_dict={self.x: batch_x, self.y: batch_y})
-                avg_cost += c / num_batches
-
-            if print_cost:
-                print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
+        self.model.save(file_path)
 
 
 def test():
-    # runs a test of the neural network constructed in the LearningPolicy object on the MNIST dataset
-
+    # runs a test of the neural network and training on the MNIST dataset
     example = TFNetwork(28 * 28, 10, 100, 3)
 
-    from tensorflow.examples.tutorials.mnist import input_data
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-    learning_rate = 0.25
+    y_train = tf.one_hot(y_train, 10).numpy()
+    y_test = tf.one_hot(y_test, 10).numpy()
+
+    # Preprocess the data (these are Numpy arrays)
+    x_train = x_train.reshape(60000, 784).astype('float32') / 255
+    x_test = x_test.reshape(10000, 784).astype('float32') / 255
+
+    y_train = y_train.astype('float32')
+    y_test = y_test.astype('float32')
+
     epochs = 10
     batch_size = 100
 
-    correct_prediction = tf.equal(tf.argmax(example.y, 1), tf.argmax(example.output, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    example.train(x_train, y_train, epochs, batch_size)
 
-    x_train, y_train = mnist.train.images, mnist.train.labels
+    result = example.model.evaluate(x_test, y_test, batch_size=batch_size, verbose=False)
 
-    example.train(x_train, y_train, epochs, learning_rate, batch_size, print_cost=True)
-
-    print("MNIST accuracy: %.3f" % example.session.run(accuracy, feed_dict={example.x: mnist.test.images, example.y: mnist.test.labels}))
+    print("Test results")
+    print("Loss: %.4f" % result[0])
+    print("Accuracy: %.4f" % result[1])
 
 
 if __name__ == "__main__":
