db2909e7c099b9a1d7bfbfacc1aa8e9fbf55ac49
masafumiendo <endoma@oregonstate.edu>
2019-11-23 23:38:19 -0800

    the latest version so far

--- traffic_generator.py
+++ traffic_generator.py
@@ -1,80 +1,31 @@
 import numpy as np
 import math
 
-# HANDLE THE GENERATION OF VEHICLES IN ONE EPISODE
+# Handle the generation of vehicles in one episode
 class TrafficGenerator:
-    def __init__(self, max_steps):
-        self._n_cars_generated = 1000  # how many cars per episode
-        self._max_steps = max_steps
 
-    # generation of routes of cars
-    def generate_routefile(self, seed):
-        np.random.seed(seed)  # make tests reproducible
+    # Public method
+    def generate_routefile(self):
 
-        # the generation of cars is distributed according to a weibull distribution
-        timings = np.random.weibull(2, self._n_cars_generated)
-        timings = np.sort(timings)
-
-        # reshape the distribution to fit the interval 0:max_steps
-        car_gen_steps = []
-        min_old = math.floor(timings[1])
-        max_old = math.ceil(timings[-1])
-        min_new = 0
-        max_new = self._max_steps
-        for value in timings:
-            car_gen_steps = np.append(car_gen_steps, ((max_new - min_new) / (max_old - min_old)) * (value - max_old) + max_new)
-
-        car_gen_steps = np.rint(car_gen_steps)  # round every value to int -> effective steps when a car will be generated
-        # produce the file for cars generation, one car per line
-        with open("data/cross_ref.rou.xml", "w") as routes:
+        with open("data/cross.rou.xml", "w") as routes:
             print("""<routes>
-            <vType accel="1.0" decel="4.5" id="standard_car" length="5.0" minGap="2.5" maxSpeed="25" sigma="0.5" />
-            <route id="W_N" edges="Wi No"/>
-            <route id="W_E" edges="Wi Eo"/>
-            <route id="W_S" edges="Wi So"/>
-            <route id="N_W" edges="Ni Wo"/>
-            <route id="N_E" edges="Ni Eo"/>
-            <route id="N_S" edges="Ni So"/>
-            <route id="E_W" edges="Ei Wo"/>
-            <route id="E_N" edges="Ei No"/>
-            <route id="E_S" edges="Ei So"/>
-            <route id="S_W" edges="Si Wo"/>
-            <route id="S_N" edges="Si No"/>
-            <route id="S_E" edges="Si Eo"/>""", file=routes)
+            <vType id="can" accel="1.5" decel="4.5" length="5.0"/>""", file=routes)
 
-            for car_counter, step in enumerate(car_gen_steps):
-                straight_or_turn = np.random.uniform()
-                if straight_or_turn < 0.75:  # choose direction: straight or turn - 75% of times the car goes straight
-                    route_straight = np.random.randint(1, 5)  # choose a random source & destination
-                    if route_straight == 1:
-                        print('    <vehicle id="W_E_%i" type="standard_car" route="W_E" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_straight == 2:
-                        print('    <vehicle id="E_W_%i" type="standard_car" route="E_W" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_straight == 3:
-                        print('    <vehicle id="N_S_%i" type="standard_car" route="N_S" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    else:
-                        print('    <vehicle id="S_N_%i" type="standard_car" route="S_N" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                else:  # car that turn -25% of the time the car turns
-                    route_turn = np.random.randint(1, 9)  # choose random source source & destination
-                    if route_turn == 1:
-                        print('    <vehicle id="W_N_%i" type="standard_car" route="W_N" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_turn == 2:
-                        print('    <vehicle id="W_S_%i" type="standard_car" route="W_S" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_turn == 3:
-                        print('    <vehicle id="N_W_%i" type="standard_car" route="N_W" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_turn == 4:
-                        print('    <vehicle id="N_E_%i" type="standard_car" route="N_E" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_turn == 5:
-                        print('    <vehicle id="E_N_%i" type="standard_car" route="E_N" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_turn == 6:
-                        print('    <vehicle id="E_S_%i" type="standard_car" route="E_S" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_turn == 7:
-                        print('    <vehicle id="S_W_%i" type="standard_car" route="S_W" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
-                    elif route_turn == 8:
-                        print('    <vehicle id="S_E_%i" type="standard_car" route="S_E" depart="%s" departLane="random" departSpeed="10" />' % (car_counter, step), file=routes)
+            print('<flow id="f12" begin="0" end="5400" from="Wi" to="Eo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f14" begin="0" end="5400" from="Wi" to="No" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f13" begin="0" end="5400" from="Wi" to="So" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f21" begin="0" end="5400" from="Ei" to="Wo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f23" begin="0" end="5400" from="Ei" to="So" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f24" begin="0" end="5400" from="Ei" to="No" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f34" begin="0" end="5400" from="Si" to="No" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f31" begin="0" end="5400" from="Si" to="Wo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f32" begin="0" end="5400" from="Si" to="Eo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f43" begin="0" end="5400" from="Ni" to="So" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f41" begin="0" end="5400" from="Ni" to="Wo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f42" begin="0" end="5400" from="Ni" to="Eo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
 
             print("</routes>", file=routes)
 
 if __name__ == '__main__':
-    traffic_generator = TrafficGenerator(5400)
-    traffic_generator.generate_routefile(32)
+    traffic_generator = TrafficGenerator()
+    traffic_generator.generate_routefile()
