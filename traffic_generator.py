8af1f9536271c5da55f46f8928862de1664fd5bd
masafumiendo <endoma@oregonstate.edu>
2019-11-24 16:54:47 -0800

    change the way to generate traffic flow

--- traffic_generator.py
+++ traffic_generator.py
@@ -1,28 +1,37 @@
-import numpy as np
-import math
+import random
 
 # Handle the generation of vehicles in one episode
 class TrafficGenerator:
 
+    def __init__(self, min_number=1000, max_number=3100, step=500):
+        self.min_number = min_number
+        self.max_number = max_number
+        self.step = step
+
     # Public method
     def generate_routefile(self):
+        
+        vehsPerHour = random.randrange(self.min_number, self.max_number, self.step)
+        print(vehsPerHour)
+        vehsPerHour_straight = int((vehsPerHour*0.5) / 4) # The number of vehicles going straight for each lane per hour
+        vehsPerHour_turning = int((vehsPerHour*0.5) / 8) # The number of vehicles turning for each lane pe hour
 
         with open("data/cross.rou.xml", "w") as routes:
             print("""<routes>
             <vType id="can" accel="1.5" decel="4.5" length="5.0"/>""", file=routes)
 
-            print('<flow id="f12" begin="0" end="5400" from="Wi" to="Eo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f14" begin="0" end="5400" from="Wi" to="No" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f13" begin="0" end="5400" from="Wi" to="So" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f21" begin="0" end="5400" from="Ei" to="Wo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f23" begin="0" end="5400" from="Ei" to="So" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f24" begin="0" end="5400" from="Ei" to="No" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f34" begin="0" end="5400" from="Si" to="No" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f31" begin="0" end="5400" from="Si" to="Wo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f32" begin="0" end="5400" from="Si" to="Eo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f43" begin="0" end="5400" from="Ni" to="So" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f41" begin="0" end="5400" from="Ni" to="Wo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
-            print('<flow id="f42" begin="0" end="5400" from="Ni" to="Eo" probability="%f"/>' % np.random.uniform(0, 1), file=routes)
+            print('<flow id="f12" begin="0" end="5400" from="Wi" to="Eo" vehsPerHour="%f"/>' % vehsPerHour_straight, file=routes)
+            print('<flow id="f14" begin="0" end="5400" from="Wi" to="No" vehsPerHour="%f"/>' % vehsPerHour_turning, file=routes)
+            print('<flow id="f13" begin="0" end="5400" from="Wi" to="So" vehsPerHour="%f"/>' % vehsPerHour_turning, file=routes)
+            print('<flow id="f21" begin="0" end="5400" from="Ei" to="Wo" vehsPerHour="%f"/>' % vehsPerHour_straight, file=routes)
+            print('<flow id="f23" begin="0" end="5400" from="Ei" to="So" vehsPerHour="%f"/>' % vehsPerHour_turning, file=routes)
+            print('<flow id="f24" begin="0" end="5400" from="Ei" to="No" vehsPerHour="%f"/>' % vehsPerHour_turning, file=routes)
+            print('<flow id="f34" begin="0" end="5400" from="Si" to="No" vehsPerHour="%f"/>' % vehsPerHour_straight, file=routes)
+            print('<flow id="f31" begin="0" end="5400" from="Si" to="Wo" vehsPerHour="%f"/>' % vehsPerHour_turning, file=routes)
+            print('<flow id="f32" begin="0" end="5400" from="Si" to="Eo" vehsPerHour="%f"/>' % vehsPerHour_turning, file=routes)
+            print('<flow id="f43" begin="0" end="5400" from="Ni" to="So" vehsPerHour="%f"/>' % vehsPerHour_straight, file=routes)
+            print('<flow id="f41" begin="0" end="5400" from="Ni" to="Wo" vehsPerHour="%f"/>' % vehsPerHour_turning, file=routes)
+            print('<flow id="f42" begin="0" end="5400" from="Ni" to="Eo" vehsPerHour="%f"/>' % vehsPerHour_turning, file=routes)
 
             print("</routes>", file=routes)
 
