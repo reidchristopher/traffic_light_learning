8dca685e9d7d274e036d7c3a76c38ee549a81448
reidchristopher <reid.christopher@yahoo.com>
2019-11-24 18:25:53 -0800

    Basic start for reinforcement training

---
+++ reinforcement_training.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from reinforcement_learner import ReinforcementLearner
+from tf_network import TFNetwork
+import numpy as np
+import sys
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+
+    main()
